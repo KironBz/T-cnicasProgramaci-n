81e22105b2c8f6f9b2206f0c3464fbd8b0ccc5ab
Bohorquez <hunab.bohorquez@ingenieria.unam.edu>
2026-03-02 10:42:22 -0600

    Interfaces

---
+++ TP/15_Interfaces.cs
@@ -0,0 +1,27 @@
+﻿//Ejemplo de interfaces
+
+// Programa Principal
+//IEjemplo algue = IEjemplo(); // No se puede declarar por que no tiene constructor, instancias una clase
+IEjemplo algo = new EjemploClase();
+algo.HacerAlgo();
+
+// Declaracion
+public interface IEjemplo
+{
+    // Definiion de Metodos
+    void HacerAlgo();
+
+    // Metodo con Implementacion
+    public void HacerAlgoMas()
+    {
+        Console.WriteLine("Haceindo ALgo Mas");
+    }
+}
+
+public class EjemploClase : IEjemplo
+{
+    public void HacerAlgo()
+    {
+        Console.WriteLine("Haciendo Algo");
+    }
+}
