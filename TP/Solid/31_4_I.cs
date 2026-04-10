6b92dcfe4c61dd9edde8b480e674ce0287bfad0a
Bohorquez <hunab.bohorquez@ingenieria.unam.edu>
2026-04-10 10:52:37 -0600

    31 parte2

---
+++ TP/Solid/31_4_I.cs
@@ -0,0 +1,39 @@
+﻿// Sin ISP
+interface IMonstruo
+{
+    void Asustar();
+    void Volar();
+    void LanzarHechizos();
+}
+
+class Fantasma : IMonstruo
+{
+    public void Asustar() => Console.WriteLine("Buhhw, te asustow, te asustow");
+    public void Volar() => Console.WriteLine("Fantasma uso levitacion");
+    public void LanzarHechizo() => throw new NotImplementedException("Los fantasmas no lanzan hechizos");
+}
+
+class Bruja : IMonstruo
+{
+    public void Asustar() => Console.WriteLine("Risa Macabra o de Cabra");
+    public void Volar() => Console.WriteLine("Vuela sobre su escoba");
+    public void LanzarHechizo() => Console.WriteLine("Te convierte en chocolate");
+}
+
+// Aplicamos ISL
+interface IAsustador { void Asustar(); }
+interface IVolador { void Volar(); }
+interface IHechicero { void LanzarHechizo(); }
+
+class FantasmaI : IAsustador, IVolador
+{
+    public void Asustar() => Console.WriteLine("Buhhw, te asustow, te asustow");
+    public void Volar() => Console.WriteLine("Fantasma uso levitacion");
+}
+
+class BrujaI : IAsustador, IVolador, IHechicero
+{
+    public void Asustar() => Console.WriteLine("Risa Macabra o de Cabra");
+    public void Volar() => Console.WriteLine("Vuela sobre su escoba");
+    public void LanzarHechizo() => Console.WriteLine("Te convierte en chocolate");
+}
