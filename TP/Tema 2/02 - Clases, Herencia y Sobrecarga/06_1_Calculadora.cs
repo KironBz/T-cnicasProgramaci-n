ee77e50ebe8625053993763f9f301b1f2858d98e
Hunab <bohorquez.atha@gmail.com>
2026-05-12 16:30:07 -0600

    tema dos 06 calculadora cientifica

---
+++ TP/Tema 2/02 - Clases, Herencia y Sobrecarga/06_1_Calculadora.cs
@@ -0,0 +1,57 @@
+﻿// Programa principal
+Calculadora calculadora = new Calculadora(5, 2);
+
+Console.WriteLine($"Los numeros a operar son: \nNumero 1 = {calculadora.Numero1} \nNumero 2 = {calculadora.Numero2} \n");
+
+int resultado = calculadora.Suma();
+Console.WriteLine($"El resultado de la suma es: {resultado}");
+
+resultado = calculadora.Resta();
+Console.WriteLine($"El resultado de la resta es: {resultado}");
+
+resultado = calculadora.Multiplicacion();
+Console.WriteLine($"El resultado de la multiplicacion es: {resultado}");
+
+float resultadoD = calculadora.Division();
+Console.WriteLine($"El resultado de la division es: {resultadoD}");
+
+// Clases
+    // Calculadora basica que opera solo 2 numeros enteros
+public class Calculadora
+{
+    // Atributos publicos (Propiedades)
+    public int Numero1 { get; set; } // Encapsulamiento
+    public int Numero2 { get; set; }
+
+    // Constructor 
+    public Calculadora (int numero1, int numero2)
+    {
+        Numero1 = numero1;
+        Numero2 = numero2;
+    }
+
+    // Metodos
+    public int Suma()
+    {
+        return Numero1 + Numero2;
+    }
+
+    public int Resta()
+    {
+        return Numero1 - Numero2;
+    }
+
+    public int Multiplicacion()
+    {
+        return Numero1 * Numero2;
+    }
+    public float Division()
+    {
+        if (Numero2==0)
+        {
+            Console.WriteLine("MATH ERROR");
+            return 0;
+        }
+        return (float) Numero1 / Numero2;
+    }
+}
--- TP/Tema 2/02 - Clases, Herencia y Sobrecarga/06_2_Calculadora .cs
+++ TP/Tema 2/02 - Clases, Herencia y Sobrecarga/06_2_Calculadora .cs
@@ -1,73 +1,4 @@
-﻿#region Calculadora Pt1
-
-/*
-
-// Programa principal
-Calculadora calculadora = new Calculadora(5, 2);
-
-Console.WriteLine($"Los numeros a operar son: \nNumero 1 = {calculadora.Numero1} \nNumero 2 = {calculadora.Numero2} \n");
-
-int resultado = calculadora.Suma();
-Console.WriteLine($"El resultado de la suma es: {resultado}");
-
-resultado = calculadora.Resta();
-Console.WriteLine($"El resultado de la resta es: {resultado}");
-
-resultado = calculadora.Multiplicacion();
-Console.WriteLine($"El resultado de la multiplicacion es: {resultado}");
-
-float resultadoD = calculadora.Division();
-Console.WriteLine($"El resultado de la division es: {resultadoD}");
-
-// Clases
-    // Calculadora basica que opera solo 2 numeros enteros
-public class Calculadora
-{
-    // Atributos publicos (Propiedades)
-    public int Numero1 { get; set; } // Encapsulamiento
-    public int Numero2 { get; set; }
-
-    // Constructor 
-    public Calculadora (int numero1, int numero2)
-    {
-        Numero1 = numero1;
-        Numero2 = numero2;
-    }
-
-    // Metodos
-    public int Suma()
-    {
-        return Numero1 + Numero2;
-    }
-
-    public int Resta()
-    {
-        return Numero1 - Numero2;
-    }
-
-    public int Multiplicacion()
-    {
-        return Numero1 * Numero2;
-    }
-    public float Division()
-    {
-        if (Numero2==0)
-        {
-            Console.WriteLine("MATH ERROR");
-            return 0;
-        }
-        return (float) Numero1 / Numero2;
-    }
-}
-
-*/
-
-#endregion
-
-#region Calculadora Avanzada
-using System.Diagnostics.CodeAnalysis;
-
-Console.WriteLine("Ingresa el primer número a operar");
+﻿Console.WriteLine("Ingresa el primer número a operar");
 int num1 = int.Parse(Console.ReadLine() ?? "");
 
 Console.WriteLine("Ingresa el segundo número a operar");
@@ -142,10 +73,8 @@ else if (sel== 2)
     }
 }
 
+//// Clases ////
 
-
-
-// Clases
 // Calculadora Básica
 public class Calculadora
 {
@@ -268,5 +197,3 @@ public class CalculadoraCientifica : Calculadora
         }
     }
 }
-
-#endregion
